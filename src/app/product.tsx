61767152781eaf23cc2731957dc77dda683e2343
Mark Kattenbelt <mark.kattenbelt@gmail.com>
2025-08-25 18:11:35 +0100

    Add product class

---
+++ src/app/product.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+interface ProductProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
+
+const Product: React.FC<ProductProps> = ({ title, description, imageUrl, price, cardSize }) => {
+  return (
+
+    <div className={`"bg-yellow-100 border border-gray-200   dark:bg-gray-800 dark:border-gray-700" ${cardSize}`} >
+      <a href="#">
+          <img className="" src={imageUrl} alt={title} />
+      </a>
+      <div className="p-5">
+          <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
+          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Free Demo
+          </a>
+          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Buy ({price})
+          </a>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
